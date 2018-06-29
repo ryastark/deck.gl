3cd519259b856397fe55f5aef9dab194829f5ecf
Xiaoji Chen <xiaoji@uber.com>
2018-06-29 10:52:58 -0700

    data filter example

---
+++ test/apps/data-filter/src/app.js
@@ -0,0 +1,75 @@
+/* global document */
+/* eslint-disable no-console */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {StaticMap} from 'react-map-gl';
+import DeckGL, {COORDINATE_SYSTEM} from 'deck.gl';
+
+import ScatterplotLayer from './scatterplot-layer';
+import {pointGrid} from './utils';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = process.env.MapboxAccessToken; // eslint-disable-line
+const INITIAL_VIEW_STATE = {
+  longitude: -122.45,
+  latitude: 37.78,
+  zoom: 11
+};
+
+class Root extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {time: 0};
+
+    this._data = pointGrid(1e6, [-1e4, -1e4, 1e4, 1e4]);
+    this._animate = this._animate.bind(this);
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  _animate() {
+    this.setState({time: Date.now() / 2000});
+    window.requestAnimationFrame(this._animate);
+  }
+
+  render() {
+    const time = this.state.time % 0.9;
+
+    const layers = [
+      new ScatterplotLayer({
+        coordinateSystem: COORDINATE_SYSTEM.METER_OFFSETS,
+        coordinateOrigin: [-122.45, 37.78],
+        data: this._data,
+
+        // Data accessors
+        getPosition: d => d,
+        getColor: d => [0, 180, 255],
+        getRadius: d => 8,
+        getFilterValue: d => (d[0] * d[0] + d[1] * d[1]) / 1e8,
+
+        // Filter
+        filterRange: [time, time + 0.1]
+      })
+    ];
+
+    return (
+      <div>
+        <DeckGL
+          controller={true}
+          initialViewState={INITIAL_VIEW_STATE}
+          layers={layers}
+        >
+          <StaticMap
+            mapStyle="mapbox://styles/uberdata/cive48w2e001a2imn5mcu2vrs"
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        </DeckGL>
+      </div>
+    );
+  }
+}
+
+render(<Root />, document.body.appendChild(document.createElement('div')));
